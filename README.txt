be27b24d81bcb3da08717e8de270fd829b1cd35b
Ania <Ania>
2017-05-03 15:40:28 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+# kodilla_zadanie_9.2
+# Java Script, Łączenie tablic, metoda push()
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var femaleNames = ['Asia', 'Basia', 'Cecylia', 'Dorota', 'Ela', 'Fela'];
+var maleNames = ['Adam', 'Bolek', 'Celestyn', 'Daniel', 'Erazm', 'Filip'];
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName ='Marian'
+
+if (allNames.indexOf(newName) === -1 ){
+    allNames.push(newName);
+}
+
+console.log(allNames);
